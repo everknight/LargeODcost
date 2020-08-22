638957198470f4e6c9acfc19dba30a53da51756d
Li, Ruiyang <lruiyang@umich.edu>
2020-08-21 22:22:30 -0500

    Updated the file format to speed up the read speed

--- organize_csv_to_search_format_plain_text.py
+++ organize_csv_to_search_format_plain_text.py
@@ -3,11 +3,11 @@ sys.path.append(r"C:\Users\lirui\OneDrive\Desktop\OD_git\LargeODcost")
 import timer_class as timer
 
 # Input file path
-#input_csv = r"D:\US_Estimated_ODMatrix\US_Estimated_ODMatrix\US_ODMatrix_3_3-6hours_Division\NA_OD_3hours.csv"
-#output_folder = r"D:\US_Estimated_ODMatrix\US_OD_data\hour03"
-#lvl = 1
+# input_csv = r"D:\US_Estimated_ODMatrix\US_Estimated_ODMatrix\US_ODMatrix_3_3-6hours_Division\NA_OD_3hours.csv"
+# output_folder = r"D:\US_Estimated_ODMatrix\US_OD_data_plain\hour03"
+# lvl = 1
 input_csv = r"D:\US_Estimated_ODMatrix\US_Estimated_ODMatrix\US_ODMatrix_3_3-6hours_Division\NA_OD_3-6hours.csv"
-output_folder = r"D:\US_Estimated_ODMatrix\US_OD_data\hour36"
+output_folder = r"D:\US_Estimated_ODMatrix\US_OD_data_plain\hour36"
 lvl = 2
 
 
@@ -16,19 +16,19 @@ def init_io (output_folder, ozcta, lvl, dzcta = ""):
     if lvl == 1:
         if not os.path.exists(output_folder + "\\" + ozcta[0:3]):
             os.mkdir(output_folder + "\\" + ozcta[0:3])
-        if os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data"):
-            temp_input = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data", "r")
-            previous_file_content = temp_input.read()
-            temp_input.close()
+        #if os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data"):
+            #temp_input = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data", "r")
+            #previous_file_content = temp_input.read()
+            #temp_input.close()
     else:
         if not os.path.exists(output_folder + "\\" + ozcta[0:3]):
             os.mkdir(output_folder + "\\" + ozcta[0:3])
         if not os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:]):
             os.mkdir(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:])
-        if os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta[0:2] + ".data"):
-            temp_input = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta[0:2] + ".data", "r")
-            previous_file_content = temp_input.read()
-            temp_input.close()
+        #if os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta[0:2] + ".data"):
+            #temp_input = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta[0:2] + ".data", "r")
+            #previous_file_content = temp_input.read()
+            #temp_input.close()
     return previous_file_content
     
 def write_current_io (current_io, lvl):
@@ -37,8 +37,8 @@ def write_current_io (current_io, lvl):
         for ozcta in current_io:
             if not os.path.exists(output_folder + "\\" + ozcta[0:3]):
                 os.mkdir(output_folder + "\\" + ozcta[0:3])
-            fout = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data", "w")
-            fout.write(str(current_io[ozcta]))
+            fout = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + ".data", "a")
+            fout.write(current_io[ozcta])
             fout.close()
     else:
         for ozcta in current_io:
@@ -47,8 +47,8 @@ def write_current_io (current_io, lvl):
             if not os.path.exists(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:]):
                 os.mkdir(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:])
             for dzcta2 in current_io[ozcta]:
-                fout = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta2 + ".data", "w")
-                fout.write(str(current_io[ozcta][dzcta2]))
+                fout = open(output_folder + "\\" + ozcta[0:3] + "\\" + ozcta[3:] + "\\" + dzcta2 + ".data", "a")
+                fout.write(current_io[ozcta][dzcta2])
                 fout.close()
 
 
@@ -83,10 +83,10 @@ while row:
                 if prev_cont != "":
                     current_io[ozcta] = ast.literal_eval(prev_cont)
                 else:
-                    current_io[ozcta] = {}    
+                    current_io[ozcta] = ""   
         if dzcta in current_io[ozcta]:
             print("{0} already in {1} dictionary!!!".format(dzcta, ozcta))
-        current_io[ozcta][dzcta] = [row_content[field_name.index("EstTime")], row_content[field_name.index("EstDist")]]  # Time, Distance   
+        current_io[ozcta]+="{0},{1},{2}\n".format(dzcta, row_content[field_name.index("EstTime")], row_content[field_name.index("EstDist")])  # Time, Distance   
     else: 
         if previous_ozcta != ozcta:
             previous_ozcta = ozcta
@@ -100,17 +100,17 @@ while row:
                 if prev_cont != "":
                     current_io[ozcta][dzcta[0:2]] = ast.literal_eval(prev_cont)
                 else:
-                    current_io[ozcta][dzcta[0:2]] = {}    
+                    current_io[ozcta][dzcta[0:2]] = ""    
             else:
                 if dzcta[0:2] not in current_io[ozcta]:
                     prev_cont = init_io(output_folder, ozcta, lvl, dzcta)
                     if prev_cont != "":
                         current_io[ozcta][dzcta[0:2]] = ast.literal_eval(prev_cont)
                     else:
-                        current_io[ozcta][dzcta[0:2]] = {}
+                        current_io[ozcta][dzcta[0:2]] = ""
         if dzcta in current_io[ozcta]:
             print("{0} already in {1} dictionary!!!".format(dzcta, ozcta))
-        current_io[ozcta][dzcta[0:2]][dzcta[2:]] = [row_content[field_name.index("EstTime")], row_content[field_name.index("EstDist")]]  # Time, Distance   
+        current_io[ozcta][dzcta[0:2]] +="{0},{1},{2}\n".format(dzcta[2:], row_content[field_name.index("EstTime")], row_content[field_name.index("EstDist")])  # Time, Distance   
         
     i += 1
     if i % 10000 == 0:
